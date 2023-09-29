87fed007158cd155fa226f510aba4e5a2d0d4fae
Chris Johnsen <chris_johnsen@pobox.com>
2023-09-29 03:41:38 -0500

    spectrogram: configurable row rate
    Move analyzer sampling to an interval instead of animation frames.
    
    Aggregate multiple samples until the row rate has ticked over.
    Aggregation is done with "max" since "average of samples" seemed to
    degrade the signal too much.
    
    Updating the canvas is still done via an animation frame. Not so much
    because it needs the better timing, but mostly to put it on another
    runtime tick.

--- src/components/SpectrogramHistory.vue
+++ src/components/SpectrogramHistory.vue
@@ -3,13 +3,19 @@ import { onMounted, onUnmounted, ref, toRef, watch, watchEffect } from 'vue'
 
 const canvasRef = ref<HTMLCanvasElement | null>(null)
 
-const props = defineProps<{
-  running: boolean
-  inputs: AudioNode[]
-  output?: AudioNode
-}>()
+const props = withDefaults(
+  defineProps<{
+    running: boolean
+    rowRate?: number // rows per second
+    inputs: AudioNode[]
+    output?: AudioNode
+  }>(),
+  { rowRate: 1 }
+)
 
 let analyzer: AnalyserNode | undefined
+let sampleIntervalId: number | undefined
+const samplePeriod = 100 // ms
 
 onMounted(() => {
   const a = new AnalyserNode(props.inputs[0].context, { fftSize: 512, smoothingTimeConstant: 0 })
@@ -46,13 +52,24 @@ onMounted(() => {
   watch(
     toRef(props, 'running'),
     (newRunning, oldRunning) => {
+      if (sampleIntervalId != null) {
+        clearInterval(sampleIntervalId)
+        sampleIntervalId = undefined
+      }
       if (!newRunning) return
-      if (!oldRunning) requestAnimationFrame(drawSpectrogram)
+      if (!oldRunning) sampleIntervalId = setInterval(gatherFrequencyData, samplePeriod)
     },
     { immediate: true }
   )
+
+  watchEffect(() => {
+    if (props.rowRate == 0) console.warn('spectrogram row-rate must not be zero')
+  })
 })
 onUnmounted(() => {
+  if (sampleIntervalId != null) clearInterval(sampleIntervalId)
+  sampleIntervalId = undefined
+
   if (!analyzer) return
 
   const a = analyzer
@@ -62,26 +79,48 @@ onUnmounted(() => {
   analyzer = undefined
 })
 
+let lastRowTime = 0
+let rowData = new Uint8Array(0)
 let frequencyData = new Uint8Array(0)
-function drawSpectrogram() {
-  if (!props.running || !analyzer || !canvasRef.value) return
-  const canvas = canvasRef.value
+
+function gatherFrequencyData() {
+  if (!props.running || !analyzer) return
+
   if (frequencyData.length != analyzer.frequencyBinCount)
     frequencyData = new Uint8Array(analyzer.frequencyBinCount)
   analyzer.getByteFrequencyData(frequencyData)
+
+  if (rowData.length != frequencyData.length) rowData = new Uint8Array(frequencyData.length)
+  for (let i = 0; i < frequencyData.length; i++) rowData[i] = Math.max(rowData[i], frequencyData[i])
+  // simple averaging drastically reduces the overall signal if the signal is not present for most of the samples that go into a row
+  // also, this accumulation needs a wider data type in rowData
+  // for (let i = 0; i < frequencyData.length; i++) rowData[i] += frequencyData[i]
+  // rowSamples++
+
+  if (props.rowRate != 0 && Date.now() - lastRowTime > 1000 / props.rowRate)
+    requestAnimationFrame(updateSpectrogram)
+}
+
+function updateSpectrogram() {
+  if (!props.running || !canvasRef.value) return
+
+  const canvas = canvasRef.value
   const ctx = canvas.getContext('2d')
   if (!ctx) return
+
   const { width, height } = canvas
   const rowHeight = 1
+  const binWidth = width / rowData.length
   ctx.drawImage(canvas, 0, 0, width, height - rowHeight, 0, rowHeight, width, height - rowHeight)
-  frequencyData.forEach((power, i) => {
-    const binStart = Math.round((width * i) / frequencyData.length)
-    const binEnd = Math.round((width * (i + 1)) / frequencyData.length)
+  rowData.forEach((power, i) => {
+    const binStart = Math.round(binWidth * i)
+    const binEnd = Math.round(binWidth * (i + 1))
     const mappedPower = 1 - Math.log2(256 - power) / 8
     ctx.fillStyle = `hsl(240deg 100% ${50 * mappedPower}%)`
     ctx.fillRect(binStart, 0, binEnd - binStart, rowHeight)
+    rowData[i] = 0
   })
-  requestAnimationFrame(drawSpectrogram)
+  lastRowTime = Date.now()
 }
 </script>
 
